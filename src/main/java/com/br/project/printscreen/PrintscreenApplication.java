598e7cff31cc59d0cd3c9eb6760de02ced081183
Mario23junior <mariojunior3251@gmail.com>
2022-03-05 03:17:09 -0300

    Iniciando projeto de printscreen

---
+++ src/main/java/com/br/project/printscreen/PrintscreenApplication.java
@@ -0,0 +1,13 @@
+package com.br.project.printscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintscreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintscreenApplication.class, args);
+	}
+
+}
