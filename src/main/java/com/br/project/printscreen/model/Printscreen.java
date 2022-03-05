a6fd22f2e7039f6e2128c275db6fbf21dbe852be
Mario23junior <mariojunior3251@gmail.com>
2022-03-05 04:03:47 -0300

    Criando modelo de dados que são usados para parametrizar valores de print

---
+++ src/main/java/com/br/project/printscreen/model/Printscreen.java
@@ -0,0 +1,81 @@
+package com.br.project.printscreen.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Printscreen {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private Integer x;
+	private Integer y;
+	private Integer f;
+	private Integer t;
+	private Integer path;
+	private String format;
+
+	public Printscreen() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getX() {
+		return x;
+	}
+
+	public void setX(Integer x) {
+		this.x = x;
+	}
+
+	public Integer getY() {
+		return y;
+	}
+
+	public void setY(Integer y) {
+		this.y = y;
+	}
+
+	public Integer getF() {
+		return f;
+	}
+
+	public void setF(Integer f) {
+		this.f = f;
+	}
+
+	public Integer getT() {
+		return t;
+	}
+
+	public void setT(Integer t) {
+		this.t = t;
+	}
+
+	public Integer getPath() {
+		return path;
+	}
+
+	public void setPath(Integer path) {
+		this.path = path;
+	}
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+}
